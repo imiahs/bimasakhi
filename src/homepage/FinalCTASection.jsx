96113eec1fc43fff6d9923884f16a5042e12ed66
Pratibha <imiahservices@gmail.com>
2026-01-26 04:03:37 +0530

    update: multiple feature and fixes

--- src/homepage/FinalCTASection.jsx
+++ src/homepage/FinalCTASection.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Button from '../ui/Button';
+import Button from '../components/ui/Button';
 
 const FinalCTASection = () => {
     return (
