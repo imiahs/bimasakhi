b599fd7fcd8053ba2a9d131b251144daeec415ae
Pratibha <imiahservices@gmail.com>
2026-02-01 00:45:26 +0530

    update: multiple feature and fixes

--- src/components/core/Header.jsx
+++ src/components/core/Header.jsx
@@ -5,7 +5,8 @@ const Header = () => {
     return (
         <header className="site-header">
             <div className="container">
-                <Link to="/" className="brand-logo"><img src="/images/home/logo.png" alt="Bima Sakhi Visual" />Bima Sakhi</Link>
+                <Link to="/" className="brand-logo">
+                    <img src="/images/home/logo.png" alt="Bima Sakhi Logo" />Bima Sakhi</Link>
                 {/* Navigation is minimal as per spec, driven by page flow */}
             </div>
         </header>
